6b251b87c3f6aef7c007fedb09643e3df25ee30e
cshyun7015 <cshyun7015@gmail.com>
2024-11-09 20:54:55 +0900

    Initialize java + gradle
    패스트캠퍼스 예제 자바 + gradle 프로젝트 셋업

---
+++ src/test/java/com/fastcampus/fastcampus_java_oop/FastcampusJavaOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fastcampus_java_oop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusJavaOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
